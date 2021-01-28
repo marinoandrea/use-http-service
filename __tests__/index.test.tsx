83b4da525a1ed6d8f57ae0f362e3ef406e78f404
Andrea Marino <am.marinoandrea@gmail.com>
2021-01-28 15:59:32 +0100

    ✅ tests init

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,172 @@
+import { renderHook } from "@testing-library/react-hooks";
+import express from "express";
+import http from "http";
+import fetch, { FetchError } from "node-fetch";
+import React from "react";
+import { act, create, ReactTestRenderer } from "react-test-renderer";
+import useHttpService from "../src";
+
+// @ts-expect-error: node-fetch args slightly differ from browser's Fetch API
+global.fetch = fetch;
+
+type SuccessResponse = {
+  msg: string;
+};
+
+type FailureResponse = {
+  errorMsg: string;
+};
+
+const SERVER_PORT = 3000;
+const API_ENDPOINT = `http://localhost:${SERVER_PORT}`;
+
+let app: express.Express;
+let serverInstance: http.Server;
+
+function initServer() {
+  app = express();
+
+  app.get("/example", (_, res) => res.send({ msg: "success!" }));
+
+  app.get("/not-authorized-endpoint", (_, res) =>
+    res.status(401).send({ errorMsg: "error!" })
+  );
+
+  app.get("/not-a-json-endpoint", (_, res) => res.send("not json"));
+
+  serverInstance = app.listen(SERVER_PORT);
+}
+
+function closeServer() {
+  serverInstance.close();
+}
+
+beforeEach(() => {
+  initServer();
+});
+
+afterEach(() => {
+  closeServer();
+});
+
+test("Request state initializes properly", () => {
+  const { result } = renderHook(() =>
+    useHttpService<undefined, SuccessResponse, FailureResponse>({
+      url: `${API_ENDPOINT}/example`,
+    })
+  );
+
+  const [requestState] = result.current;
+
+  expect(requestState.isPending).toBe(false);
+  expect(requestState.isSuccess).toBe(false);
+});
+
+test("Request returns SuccessResult when HTTP code indicates success", async () => {
+  const { result } = renderHook(() =>
+    useHttpService<undefined, SuccessResponse, FailureResponse>({
+      url: `${API_ENDPOINT}/example`,
+    })
+  );
+
+  const [, getData] = result.current;
+
+  expect.hasAssertions();
+
+  await act(async () => {
+    const data = await getData();
+    expect(data).toStrictEqual({ isOk: true, data: { msg: "success!" } });
+  });
+});
+
+test("Request returns ErrorResult when HTTP code indicates failure", async () => {
+  const { result } = renderHook(() =>
+    useHttpService<undefined, SuccessResponse, FailureResponse>({
+      url: `${API_ENDPOINT}/not-authorized-endpoint`,
+    })
+  );
+
+  const [, getData] = result.current;
+
+  expect.hasAssertions();
+
+  await act(async () => {
+    const error = await getData();
+    expect(error).toStrictEqual({ isOk: false, error: { errorMsg: "error!" } });
+  });
+});
+
+test("Request throws FetchError when response is not valid JSON", async () => {
+  const { result } = renderHook(() =>
+    useHttpService<undefined, SuccessResponse, FailureResponse>({
+      url: `${API_ENDPOINT}/not-a-json-endpoint`,
+    })
+  );
+
+  const [, getData] = result.current;
+
+  expect.hasAssertions();
+
+  await act(async () => {
+    try {
+      await getData();
+    } catch (e) {
+      expect(e).toBeInstanceOf(FetchError);
+    }
+  });
+});
+
+test("Component displays success response body correctly.", async () => {
+  let component: ReactTestRenderer;
+  act(() => {
+    component = create(<TestComponent />);
+  });
+
+  const instance = component!.root;
+
+  const button = instance.findByType("button");
+  await act(async () => await button.props.onClick());
+
+  const testDiv = instance!.findByType("div");
+  expect(testDiv.children[0]).toBe("success!");
+});
+
+test("Component displays failure response body correctly.", async () => {
+  let component: ReactTestRenderer;
+  act(() => {
+    component = create(<TestComponent failure />);
+  });
+
+  const instance = component!.root;
+
+  const button = instance.findByType("button");
+  await act(async () => await button.props.onClick());
+
+  const testDiv = instance!.findByType("div");
+  expect(testDiv.children[0]).toBe("error!");
+});
+
+const TestComponent: React.FC<{ failure?: boolean }> = ({
+  failure = false,
+}) => {
+  const [{ data, error, isPending, isSuccess }, callApi] = useHttpService<
+    undefined,
+    SuccessResponse,
+    FailureResponse
+  >({
+    url: `${API_ENDPOINT}/${failure ? "not-authorized-endpoint" : "example"}`,
+  });
+
+  const handleClick = async () => await callApi();
+
+  if (isPending) return <div id="test">Loading...</div>;
+
+  if (!isSuccess && error) return <div id="test">{error.errorMsg}</div>;
+
+  return (
+    <>
+      <button onClick={handleClick}>Click</button>
+      <div id="test">{data && data.msg}</div>
+    </>
+  );
+};
