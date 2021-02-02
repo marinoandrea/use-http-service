d2490f8ed3e5f1bfac745d86b305bbd8ad0b0225
Andrea Marino <am.marinoandrea@gmail.com>
2021-02-02 23:37:04 +0100

    🔧 jest config update

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx$",
+  coveragePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
 };
